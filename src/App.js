755c9ef68d01fa23d9e0cb90185713228b4377bc
Isabella da Silva Gonçalves <161075154+IsabellaSGoncalves@users.noreply.github.com>
2025-03-19 09:53:11 -0300

    Lógica do Jogo da Velha
    Commit onde a lógica do jogo da velha é adicionada através de classes.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react'
 function App() {
+
+  class Tabuleiro extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      }
+    }
+
+    handleClick(i){
+      const squares = this.state.squares.slice()
+      if(calculateWinner(squares) || squares[i]){
+        return
+      }
+      squares[i] = this.state.xIsNext ? 'X': 'O'
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      })
+    }
+    renderSquare(i){
+      return <Square value={this.state.squares[i]}
+       onClick={()=> this.handleClick(i)}/>
+    }
+    render(){
+      const winner = calculateWinner(this.state.squares)
+      let status
+      if(winner){
+        status= 'Vencedor: ' + winner
+      } else {
+        status ='Próximo jogador: ' + (this.state.xIsNext ? 'X' : 'O')
+      }
+
+      return(
+        <div>
+          <div className='status'>{status}</div>
+          <div className='board-row'>
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className='board-row'>
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className='board-row'>
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      )
+    }
+    
+  }
+    class Square extends React.Component {
+      render(){
+        return ( 
+          <button 
+          className="square" 
+          onClick={() => this.props.onClick()}>
+          {this.props.value}
+          </button>
+        )
+      }
+    }
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ]
+  for (let i = 0; i< lines.length; i++){
+    const [a,b,c ] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares [a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tabuleiro/>
+      <Square/>
     </div>
   );
 }
+
+
 
 export default App;
